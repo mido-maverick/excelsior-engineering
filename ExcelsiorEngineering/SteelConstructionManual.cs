9ae14729131e17d8790cfdaa02c6319c86eb1336
Mido <75834715+mido-maverick@users.noreply.github.com>
2025-10-06 15:03:49 +0800

    Create SteelConstructionManual.cs

---
+++ ExcelsiorEngineering/SteelConstructionManual.cs
@@ -0,0 +1,42 @@
+﻿using UnitsNet;
+
+namespace ExcelsiorEngineering;
+
+public static class SteelConstructionManual
+{
+    public static class BeamFormulas
+    {
+        public static class SimpleBeam
+        {
+            public static class UniformlyDistributedLoad
+            {
+                public static Force Vmax(ForcePerLength w, Length l) => (w * l) / 2;
+                public static Torque Mmax(ForcePerLength w, Length l) => (w * (l * l)) / 8;
+                public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) => throw new NotImplementedException();
+            }
+        }
+
+        public static class CantileverBeam
+        {
+            public static class UniformlyDistributedLoad
+            {
+                public static Force Vmax(ForcePerLength w, Length l) => w * l;
+                public static Torque Mmax(ForcePerLength w, Length l) => (w * (l * l)) / 2;
+                public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) => throw new NotImplementedException();
+            }
+        }
+
+        public static class ContinuousBeam
+        {
+            public static class ThreeEqualSpans
+            {
+                public static class AllSpansLoaded
+                {
+                    public static Force Vmax(ForcePerLength w, Length l) => 0.6 * (w * l);
+                    public static Torque Mmax(ForcePerLength w, Length l) => 0.1 * (w * (l * l));
+                    public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) => throw new NotImplementedException();
+                }
+            }
+        }
+    }
+}
