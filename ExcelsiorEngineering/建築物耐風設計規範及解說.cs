3f0573889fb765df24faca36821ac5d8d0633961
Mido <75834715+mido-maverick@users.noreply.github.com>
2025-10-06 16:50:45 +0800

    Add national regulation static classes

---
+++ ExcelsiorEngineering/建築物耐風設計規範及解說.cs
@@ -0,0 +1,29 @@
+﻿namespace ExcelsiorEngineering;
+
+/// <summary>
+/// </summary>
+/// <remarks>
+///     <list type="bullet">
+///         <item>
+///             <see href="https://www.nlma.gov.tw/ch/legislation/regsearch/166">
+///                 建築物耐風設計規範及解說 | 內政部國土管理署全球資訊網
+///             </see>
+///         </item>
+///         <item>
+///             <see href="https://glrs.moi.gov.tw/LawContent.aspx?id=GL000670">
+///                 內政部主管法規查詢系統-法規內容-建築物耐風設計規範及解說
+///             </see>
+///         </item>
+///     </list>
+/// </remarks>
+public static class 建築物耐風設計規範及解說
+{
+    /// <remarks>
+    ///     <see href="https://www.nlma.gov.tw/uploads/files/ed1498837647323cbd8cd287ce939972.pdf">
+    ///         木構造建築物設計及施工技術規範 第二章 建築物設計風力之計算
+    ///     </see>
+    /// </remarks>
+    public static class 建築物設計風力之計算
+    {
+    }
+}
---
+++ ExcelsiorEngineering/木構造建築物設計及施工技術規範.cs
@@ -0,0 +1,29 @@
+﻿namespace ExcelsiorEngineering;
+
+/// <summary>
+/// </summary>
+/// <remarks>
+///     <list type="bullet">
+///         <item>
+///             <see href="https://www.nlma.gov.tw/ch/legislation/regsearch/960">
+///                 木構造建築物設計及施工技術規範 | 內政部國土管理署全球資訊網
+///             </see>
+///         </item>
+///         <item>
+///             <see href="https://glrs.moi.gov.tw/LawContent.aspx?id=FL025530">
+///                 內政部主管法規查詢系統-法規內容-木構造建築物設計及施工技術規範
+///             </see>
+///         </item>
+///     </list>
+/// </remarks>
+public static class 木構造建築物設計及施工技術規範
+{
+    /// <remarks>
+    ///     <see href="https://www.nlma.gov.tw/uploads/files/c3069fb4ce95f330816455e1f10e5b11.pdf">
+    ///         木構造建築物設計及施工技術規範 第四章 材料及容許應力
+    ///     </see>
+    /// </remarks>
+    public static class 材料及容許應力
+    {
+    }
+}
