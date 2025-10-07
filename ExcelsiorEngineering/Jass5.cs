9cb5297c2111137b0d20016ee88e9c4c43978e6f
Mido Maverick <middle891129tw@gmail.com>
2025-10-07 16:31:35 +0800

    Create Jass5.cs

---
+++ ExcelsiorEngineering/Jass5.cs
@@ -0,0 +1,13 @@
+﻿namespace ExcelsiorEngineering;
+
+/// <summary>
+///     Japanese Architectural Standard Specification (JASS 5) for Reinforced Concrete Work
+/// </summary>
+/// <remarks>
+///     <see href="https://www.aij.or.jp/jpn/databox/2020/public_jass5-01.pdf">
+///         建築工事標準仕様書・同解説 JASS 5 鉄筋コンクリート工事
+///     </see>
+/// </remarks>
+public static class Jass5
+{
+}
