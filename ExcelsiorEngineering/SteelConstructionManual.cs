d1ccf66afd7779940b57f1eeccaae3e2770fdf8b
Mido Maverick <middle891129tw@gmail.com>
2025-10-15 14:29:35 +0800

    Implement Δmax calculation for beam cases

--- ExcelsiorEngineering/SteelConstructionManual.cs
+++ ExcelsiorEngineering/SteelConstructionManual.cs
@@ -12,7 +12,8 @@ public static class SteelConstructionManual
             {
                 public static Force Vmax(ForcePerLength w, Length l) => (w * l) / 2;
                 public static Torque Mmax(ForcePerLength w, Length l) => (w * (l * l)) / 8;
-                public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) => throw new NotImplementedException();
+                public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) => // (5 * w * (l * l * l * l)) / (384 * E * I)
+                    Length.FromMeters((5 * w.NewtonsPerMeter * Math.Pow(l.Meters, 4)) / (384 * E.Pascals * I.MetersToTheFourth));
             }
         }
 
@@ -22,7 +23,8 @@ public static class SteelConstructionManual
             {
                 public static Force Vmax(ForcePerLength w, Length l) => w * l;
                 public static Torque Mmax(ForcePerLength w, Length l) => (w * (l * l)) / 2;
-                public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) => throw new NotImplementedException();
+                public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) => // (w * (l * l * l * l)) / (8 * E * I)
+                    Length.FromMeters((w.NewtonsPerMeter * Math.Pow(l.Meters, 4)) / (8 * E.Pascals * I.MetersToTheFourth));
             }
         }
 
@@ -34,7 +36,8 @@ public static class SteelConstructionManual
                 {
                     public static Force Vmax(ForcePerLength w, Length l) => 0.6 * (w * l);
                     public static Torque Mmax(ForcePerLength w, Length l) => 0.1 * (w * (l * l));
-                    public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) => throw new NotImplementedException();
+                    public static Length Δmax(ForcePerLength w, Length l, Pressure E, AreaMomentOfInertia I) => // (w * (l * l * l * l)) / (145 * E * I)
+                        Length.FromMeters((w.NewtonsPerMeter * Math.Pow(l.Meters, 4)) / (145 * E.Pascals * I.MetersToTheFourth));
                 }
             }
         }
