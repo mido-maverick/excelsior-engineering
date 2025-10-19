1af08edba8b302d3d92785ddf6a6a33391b2cb28
Mido <75834715+mido-maverick@users.noreply.github.com>
2025-10-19 21:47:45 +0800

    Add QuantityCheck class

---
+++ EngineeringFort/QuantityCheck.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+using UnitsNet;
+
+namespace EngineeringFort;
+
+public record class QuantityCheck<TQuantity> : Check
+    where TQuantity : IQuantity, IComparisonOperators<TQuantity, TQuantity, bool>, new()
+{
+    public TQuantity Value { get; set; } = new();
+    public TQuantity Limit { get; set; } = new();
+    public override bool IsValid => Value < Limit;
+}
