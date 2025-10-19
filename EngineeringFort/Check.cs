31d60f600bdffe4dcff5b066688d3fa09e8f0d9b
Mido <75834715+mido-maverick@users.noreply.github.com>
2025-10-19 15:58:48 +0800

    Add ICheck interface and abstract Check record

---
+++ EngineeringFort/Check.cs
@@ -0,0 +1,15 @@
+﻿namespace EngineeringFort;
+
+public interface ICheck
+{
+    ICheck[] SubChecks { get; }
+    bool IsValid { get; }
+    string CheckStatus { get; }
+}
+
+public abstract record class Check : ICheck
+{
+    public virtual ICheck[] SubChecks { get; } = [];
+    public virtual bool IsValid => SubChecks.All(sc => sc.IsValid);
+    public virtual string CheckStatus => IsValid ? "<<OK>>" : "--NG--";
+}
