c9ceedc27a83849ad245d52afa95a95dbad5d969
Mido <75834715+mido-maverick@users.noreply.github.com>
2025-10-20 21:40:22 +0800

    Replace Chinese names with English ones

---
+++ EngineeringFort/CrossSection.cs
@@ -0,0 +1,23 @@
+﻿using UnitsNet;
+
+namespace EngineeringFort;
+
+public interface ICrossSection
+{
+    Area CrossSectionalArea { get; }
+    Volume SectionModulus { get; }
+    AreaMomentOfInertia MomentOfInertia { get; }
+}
+
+public record class RectangularCrossSection : ICrossSection
+{
+    public static Area CalculateCrossSectionalArea(Length w, Length h) => w * h;
+    public static Volume CalculateSectionModulus(Length w, Length h) => w * h * h / 6;
+    public static AreaMomentOfInertia CalculateMomentOfInertia(Length w, Length h) => throw new NotImplementedException();
+
+    public Length Width { get; set; }
+    public Length Height { get; set; }
+    public Area CrossSectionalArea => CalculateCrossSectionalArea(Width, Height);
+    public Volume SectionModulus => CalculateSectionModulus(Width, Height);
+    public AreaMomentOfInertia MomentOfInertia => CalculateMomentOfInertia(Width, Height);
+}
