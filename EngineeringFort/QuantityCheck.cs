a185bb2a35344fb8bd95d6777265a8818cae763b
Mido Maverick <middle891129tw@gmail.com>
2025-10-20 14:29:56 +0800

    Make Value and Limit properties virtual

--- EngineeringFort/QuantityCheck.cs
+++ EngineeringFort/QuantityCheck.cs
@@ -6,7 +6,7 @@ namespace EngineeringFort;
 public record class QuantityCheck<TQuantity> : Check
     where TQuantity : IQuantity, IComparisonOperators<TQuantity, TQuantity, bool>, new()
 {
-    public TQuantity Value { get; set; } = new();
-    public TQuantity Limit { get; set; } = new();
+    public virtual TQuantity Value { get; set; } = new();
+    public virtual TQuantity Limit { get; set; } = new();
     public override bool IsValid => Value < Limit;
 }
