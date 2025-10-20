a81841955ffc50d6769587476b51077b12b7e2e5
Mido <75834715+mido-maverick@users.noreply.github.com>
2025-10-20 22:34:54 +0800

    Implement moment of inertia for rectangular section

--- EngineeringFort/CrossSection.cs
+++ EngineeringFort/CrossSection.cs
@@ -13,7 +13,8 @@ public record class RectangularCrossSection : ICrossSection
 {
     public static Area CalculateCrossSectionalArea(Length w, Length h) => w * h;
     public static Volume CalculateSectionModulus(Length w, Length h) => w * h * h / 6;
-    public static AreaMomentOfInertia CalculateMomentOfInertia(Length w, Length h) => throw new NotImplementedException();
+    public static AreaMomentOfInertia CalculateMomentOfInertia(Length w, Length h) => // w * h * h * h / 12
+        AreaMomentOfInertia.FromMetersToTheFourth(w.Meters * Math.Pow(h.Meters, 3) / 12);
 
     public Length Width { get; set; }
     public Length Height { get; set; }
