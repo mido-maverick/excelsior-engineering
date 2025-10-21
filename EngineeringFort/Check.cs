b456f91bb7db1a3da90c3263397f36d2e2902116
Mido Maverick <middle891129tw@gmail.com>
2025-10-21 17:25:40 +0800

    Refactor SubChecks to use IEnumerable<ICheck>

--- EngineeringFort/Check.cs
+++ EngineeringFort/Check.cs
@@ -2,14 +2,14 @@
 
 public interface ICheck
 {
-    ICheck[] SubChecks { get; }
+    IEnumerable<ICheck> SubChecks { get; }
     bool IsValid { get; }
     string CheckStatus { get; }
 }
 
 public abstract record class Check : ICheck
 {
-    public virtual ICheck[] SubChecks { get; } = [];
+    public virtual IEnumerable<ICheck> SubChecks { get; } = [];
     public virtual bool IsValid => SubChecks.All(sc => sc.IsValid);
     public virtual string CheckStatus => IsValid ? "<<OK>>" : "--NG--";
 }
