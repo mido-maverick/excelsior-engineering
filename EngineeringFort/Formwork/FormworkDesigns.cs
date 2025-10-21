c6e25eafd770306a8de692c530a8008db20c23f5
Mido Maverick <middle891129tw@gmail.com>
2025-10-21 17:38:57 +0800

    Add FormworkLayerChecks to SideFormworkDesign

--- EngineeringFort/Formwork/FormworkDesigns.cs
+++ EngineeringFort/Formwork/FormworkDesigns.cs
@@ -12,4 +12,8 @@ public interface IBottomFormworkDesign
 public record class SideFormworkDesign : FormworkDesign, ISideFormworkDesign
 {
     public virtual Pressure MaxSidePressure { get; set; }
+
+    public FormworkLayerCheck?[] FormworkLayerChecks { get; } = new FormworkLayerCheck?[5];
+
+    public override IEnumerable<ICheck> SubChecks => FormworkLayerChecks.OfType<ICheck>();
 }
