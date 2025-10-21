53f6159df7abb8d81ad97b58decc825204c72f43
Mido <75834715+mido-maverick@users.noreply.github.com>
2025-10-22 00:38:25 +0800

    Add CrossSection property to FormworkSupport

--- EngineeringFort/Formwork/FormworkSupport.cs
+++ EngineeringFort/Formwork/FormworkSupport.cs
@@ -2,4 +2,5 @@ namespace EngineeringFort.Formwork;
 
 public record class FormworkSupport : FormworkComponent
 {
+    public virtual ICrossSection CrossSection { get; } = new RectangularCrossSection();
 }
