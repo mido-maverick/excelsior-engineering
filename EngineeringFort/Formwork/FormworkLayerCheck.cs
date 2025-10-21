db46ba7d6b879885a57ae1941a3c9532c1beeef8
Mido Maverick <middle891129tw@gmail.com>
2025-10-21 18:12:06 +0800

    Add FormworkComponent property

--- EngineeringFort/Formwork/FormworkLayerCheck.cs
+++ EngineeringFort/Formwork/FormworkLayerCheck.cs
@@ -3,4 +3,6 @@ namespace EngineeringFort.Formwork;
 public record class FormworkLayerCheck : Check
 {
     public virtual Pressure Pressure { get; set; }
+
+    public FormworkComponent? FormworkComponent { get; set; }
 }
