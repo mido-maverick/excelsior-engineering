46851c757b72b31c6af69c8c1659ec743f98d8a6
Mido Maverick <middle891129tw@gmail.com>
2025-10-22 10:02:18 +0800

    Create FormworkLayerCheck record classes

--- EngineeringFort/Formwork/FormworkLayerCheck.cs
+++ EngineeringFort/Formwork/FormworkLayerCheck.cs
@@ -9,3 +9,15 @@ public record class FormworkLayerCheck<T> : FormworkLayerCheck where T : Formwor
 {
     public virtual T? FormworkComponent { get; set; }
 }
+
+public record class FormworkSheathingLayerCheck : FormworkLayerCheck<FormworkSheathing>
+{
+}
+
+public record class FormworkSupportLayerCheck : FormworkLayerCheck<FormworkSupport>
+{
+}
+
+public record class FormworkTieRodLayerCheck : FormworkLayerCheck<FormworkTieRod>
+{
+}
