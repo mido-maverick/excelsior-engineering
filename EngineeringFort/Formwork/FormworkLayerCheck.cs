7c17bce26f1a6bd00d8a6b71c3195a4f081c4dd2
Mido Maverick <middle891129tw@gmail.com>
2025-10-22 13:46:21 +0800

    Add MaximumTensileStress property to layer checks

--- EngineeringFort/Formwork/FormworkLayerCheck.cs
+++ EngineeringFort/Formwork/FormworkLayerCheck.cs
@@ -22,16 +22,25 @@ public record class FormworkSheathingLayerCheck : FormworkLayerCheck<FormworkShe
         Pressure.KilogramsForcePerSquareCentimeter * UnitStripWidth.Centimeters);
 
     public virtual Torque MaximumBendingMoment => SimpleBeam.UniformlyDistributedLoad.Mmax(UniformlyDistributedLoad, SupportSpacing);
+
+    public virtual Pressure MaximumTensileStress => Pressure.FromKilogramsForcePerSquareCentimeter(
+        MaximumBendingMoment.KilogramForceCentimeters /
+        RectangularCrossSection.CalculateSectionModulus(UnitStripWidth, FormworkComponent.Thickness).CubicCentimeters);
 }
 
 public record class FormworkSupportLayerCheck : FormworkLayerCheck<FormworkSupport>
 {
+    public virtual Length SupportSpacing { get; set; }
+
     public virtual Length TributaryWidth { get; set; }
 
     public virtual ForcePerLength UniformlyDistributedLoad => ForcePerLength.FromKilogramsForcePerCentimeter(
         Pressure.KilogramsForcePerSquareCentimeter * TributaryWidth.Centimeters);
 
-    public virtual Torque MaximumBendingMoment => ContinuousBeam.ThreeEqualSpans.AllSpansLoaded.Mmax(UniformlyDistributedLoad, TributaryWidth);
+    public virtual Torque MaximumBendingMoment => ContinuousBeam.ThreeEqualSpans.AllSpansLoaded.Mmax(UniformlyDistributedLoad, SupportSpacing);
+
+    public virtual Pressure MaximumTensileStress => Pressure.FromKilogramsForcePerSquareCentimeter(
+        MaximumBendingMoment.KilogramForceCentimeters / FormworkComponent.CrossSection.SectionModulus.CubicCentimeters);
 }
 
 public record class FormworkTieRodLayerCheck : FormworkLayerCheck<FormworkTieRod>
--- EngineeringFort/Formwork/FormworkSheathing.cs
+++ EngineeringFort/Formwork/FormworkSheathing.cs
@@ -2,4 +2,5 @@ namespace EngineeringFort.Formwork;
 
 public record class FormworkSheathing : FormworkComponent
 {
+    public virtual Length Thickness { get; set; }
 }
