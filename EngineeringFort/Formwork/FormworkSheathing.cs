850e063b33273c0eec6d266e081e48510a4d7fe0
Mido Maverick <middle891129tw@gmail.com>
2025-10-22 17:42:30 +0800

    Add ElasticModulus property to FormworkSheathing

--- EngineeringFort/Formwork/FormworkSheathing.cs
+++ EngineeringFort/Formwork/FormworkSheathing.cs
@@ -2,6 +2,8 @@ namespace EngineeringFort.Formwork;
 
 public record class FormworkSheathing : FormworkComponent
 {
+    public virtual Pressure ElasticModulus { get; set; }
+
     public virtual Length Thickness { get; set; }
 
     public virtual IFormworkSheathingMaterial? Material { get; set; }
