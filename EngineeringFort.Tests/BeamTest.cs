b12214684bad8d7f281ec94804b6db9b1a93fa00
Mido Maverick <middle891129tw@gmail.com>
2025-10-28 16:48:22 +0800

    Add new test cases for Beam regex pattern

--- EngineeringFort.Tests/BeamTest.cs
+++ EngineeringFort.Tests/BeamTest.cs
@@ -6,6 +6,7 @@ public class BeamTest
 {
     [Theory]
     [InlineData(false, "")]
+    [InlineData(false, "'")]
     [InlineData(false, "1")]
     [InlineData(false, "G")]
     [InlineData(true, "G1")]
@@ -18,6 +19,9 @@ public class BeamTest
     [InlineData(true, "CG1")]
     [InlineData(true, "CG1a")]
     [InlineData(true, "G1a")]
+    [InlineData(true, "G1'")]
+    [InlineData(true, "G1'-1")]
+    [InlineData(true, "G1-1")]
     public void Beam_RegexPattern_ShouldBeCorrect(bool expected, string str)
     {
         Assert.Equal(expected, Regex.Match(str, Beam.RegexPattern).Success);
