f910994073b7b483a5295919357b7343bbb55637
Mido Maverick <middle891129tw@gmail.com>
2025-10-30 15:24:30 +0800

    Refactor formwork support elastic modulus

--- EngineeringFort/Formwork/FormworkSupport.cs
+++ EngineeringFort/Formwork/FormworkSupport.cs
@@ -2,11 +2,11 @@ namespace EngineeringFort.Formwork;
 
 public record class FormworkSupport : FormworkComponent
 {
-    public virtual Pressure ElasticModulus { get; set; }
-
     public virtual ICrossSection CrossSection { get; } = new RectangularCrossSection();
 
     public virtual IFormworkSupportMaterial? Material { get; set; }
 
     public virtual Pressure? AllowableBendingStress => Material?.AllowableBendingStress();
+
+    public virtual Pressure? ElasticModulus => Material?.ElasticModulus();
 }
