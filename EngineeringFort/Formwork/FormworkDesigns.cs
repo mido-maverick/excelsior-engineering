98523f35e6f2d9fac79439a77a495ae5c9317d93
Mido <75834715+mido-maverick@users.noreply.github.com>
2025-11-01 22:00:38 +0800

    Rename MaxSidePressure to MaximumSidePressure

--- EngineeringFort/Formwork/FormworkDesigns.cs
+++ EngineeringFort/Formwork/FormworkDesigns.cs
@@ -2,7 +2,7 @@ namespace EngineeringFort.Formwork;
 
 public interface ISideFormworkDesign
 {
-    Pressure MaxSidePressure { get; set; }
+    Pressure MaximumSidePressure { get; set; }
 }
 
 public interface IBottomFormworkDesign
@@ -12,7 +12,7 @@ public interface IBottomFormworkDesign
 [Display(Name = nameof(SideFormworkDesign), ResourceType = typeof(DisplayStrings))]
 public record class SideFormworkDesign : FormworkDesign, ISideFormworkDesign
 {
-    public virtual Pressure MaxSidePressure { get; set; }
+    public virtual Pressure MaximumSidePressure { get; set; }
 
     [Display(Name = nameof(FormworkLayerCheck), ResourceType = typeof(DisplayStrings))]
     public FormworkLayerCheck?[] FormworkLayerChecks { get; } = new FormworkLayerCheck?[5];
