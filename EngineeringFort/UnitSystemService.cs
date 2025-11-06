0ec65cc60cca01f001506c5fd81dec5d57c1417c
Mido Maverick <middle891129tw@gmail.com>
2025-11-06 17:36:27 +0800

    Add TypeMemberActiveUnits property

--- EngineeringFort/UnitSystemService.cs
+++ EngineeringFort/UnitSystemService.cs
@@ -3,4 +3,6 @@ namespace EngineeringFort;
 public class UnitSystemService
 {
     public List<string> ActiveUnits { get; } = [];
+
+    public Dictionary<MemberInfo, string[]> TypeMemberActiveUnits { get; } = [];
 }
