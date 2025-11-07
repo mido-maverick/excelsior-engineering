300edea7f80b297ea20a85d446eeeabfcc817db5
Mido Maverick <middle891129tw@gmail.com>
2025-11-07 15:16:02 +0800

    Remove TypeMemberActiveUnits property

--- EngineeringFort/UnitSystemService.cs
+++ EngineeringFort/UnitSystemService.cs
@@ -3,6 +3,4 @@ namespace EngineeringFort;
 public class UnitSystemService
 {
     public List<string> ActiveUnits { get; } = [];
-
-    public Dictionary<MemberInfo, string[]> TypeMemberActiveUnits { get; } = [];
 }
