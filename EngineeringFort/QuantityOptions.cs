851af523c07511b84190ac6914662a76d6f32100
Mido Maverick <middle891129tw@gmail.com>
2025-11-07 10:34:24 +0800

    Create QuantityOptions class

---
+++ EngineeringFort/QuantityOptions.cs
@@ -0,0 +1,8 @@
+namespace EngineeringFort;
+
+public class QuantityOptions
+{
+    public string[] DefaultUnits { get; init; } = [];
+
+    public Dictionary<string, string[]> Formats { get; init; } = [];
+}
