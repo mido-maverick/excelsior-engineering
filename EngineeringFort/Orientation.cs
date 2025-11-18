14ebdce340bcc4af7960b4a6453aac0d7d3950e1
Mido Maverick <middle891129tw@gmail.com>
2025-11-18 17:49:36 +0800

    Add display attributes to Orientation enum

--- EngineeringFort/Orientation.cs
+++ EngineeringFort/Orientation.cs
@@ -1,3 +1,10 @@
 namespace EngineeringFort;
 
-public enum Orientation { Horizontal, Vertical }
+public enum Orientation
+{
+    [Display(Name = nameof(DisplayStrings.HorizontalOrientation), ResourceType = typeof(DisplayStrings))]
+    Horizontal,
+
+    [Display(Name = nameof(DisplayStrings.VerticalOrientation), ResourceType = typeof(DisplayStrings))]
+    Vertical
+}
